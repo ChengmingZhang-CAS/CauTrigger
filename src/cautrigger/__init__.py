6cba7dda38c7b456e86e2154aab7da86a61dda97
ChengmingZhang-CAS <zhangchengming2017@sibcb.ac.cn>
2026-03-07 22:11:19 +0900

    Support uncertain labels in state loss

--- src/cautrigger/__init__.py
+++ src/cautrigger/__init__.py
@@ -1,9 +1,9 @@
-# from .model import CauTrigger1L, CauTrigger2L, CauTrigger3L
-# from .causaleffect import joint_uncond_v2, beta_info_flow_v2
-# from .utils import pert_plot_vector_field, pert_plot_stream
-#
-# __all__ = ["CauTrigger1L", "CauTrigger2L", "CauTrigger3L", "joint_uncond_v2", "beta_info_flow_v2", "pert_plot_vector_field", "pert_plot_stream"]
-#
-# from importlib.metadata import version
-#
-# __version__ = version("cautrigger")
+from .model import CauTrigger1L, CauTrigger2L, CauTrigger3L
+from .causaleffect import joint_uncond_v2, beta_info_flow_v2
+from .utils import pert_plot_vector_field, pert_plot_stream
+
+__all__ = ["CauTrigger1L", "CauTrigger2L", "CauTrigger3L", "joint_uncond_v2", "beta_info_flow_v2", "pert_plot_vector_field", "pert_plot_stream"]
+
+from importlib.metadata import version
+
+__version__ = version("cautrigger")
